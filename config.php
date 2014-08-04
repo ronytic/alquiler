d6a4df1e428d64e99ccf246ca7472d9e4f1e7a71
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-08-03 22:17:39 -0400

    Corrección de Configuración

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ $title=utf8_encode("Sistema de Control de Alquiler");
 $siglas="CSB";
 $directory="alquiler";//ej:carp/ sin el "/"
 $url="http://11.1.2.31/";//ej:http:"localhost/"
-$gestion=utf8_encode("2012");
-$lema=utf8_encode("\"Mente Sana Cuerpo Sano\"");
-$anio="2012";
+$gestion=date("Y");
+$lema=utf8_encode("\"Sistema de Control de Alquiler\"");
+$anio="2014";
 ?>
