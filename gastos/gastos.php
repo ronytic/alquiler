41684bbf98f5b0bc59d3dfbebcea7451111aea34
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-08-03 20:16:17 -0400

    Corregido Registro de Gasto

--- gastos/gastos.php
+++ gastos/gastos.php
@@ -15,7 +15,7 @@ include_once("../cabecerahtml.php");
         <table>
         <tr><td>Detalle del Gasto:</td><td><input type="text" name="detalle" size="50"/></td></tr>
         <tr><td>Monto Gasto:</td><td><input type="number" name="monto" autocomplete="off" min="0" step="1" class="der"/>Bs.</td></tr>
-        <tr><td>Fecha de Gasto:</td><td><input type="date" name="fecha" /></td></tr>
+        <tr><td>Fecha de Gasto:</td><td><input type="date" name="fecha" value="<?php echo date("Y-m-d")?>"/></td></tr>
  
         <tr><td>Observaciones:</td><td><textarea name="observaciones" rows="5" cols="30"></textarea></td></tr>
         <tr><td></td><td><input type="submit" value="Registrar" class="corner-all"><input type="reset" class="corner-all"></td></tr>
